271d482f534345e93f5f3738f26fbc31c7e79c62
root <cku0828@gmail.com>
2016-06-10 16:46:31 +0900

    test

--- src/test.java
+++ src/test.java
@@ -4,7 +4,7 @@
 public class test {
     public static void main(String[] args) {
         int i = 0;
-        while(i < 5){
+        while(i < 6){
             makeThread mt = new makeThread();
             i++;
         }
